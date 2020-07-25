040ca0c87249b3e8818af6751c55bd9fae89e6e4
Krzysztof Dziardziel <krzysztof.dziardziel@students.akademeia.edu.pl>
2020-07-25 03:08:06 +0200

    Added looping over all data

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geophone/data.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
-
-// List<DataPoint> points = new List();
-// dataToPoints(List data) {
-//   for (int i = 0; i < 1023; i++) {
-//     points.add(DataPoint<int>(value: data[i], xAxis: i));
-//   }
-//   return points;
-// }
-
-// ignore: must_be_immutable
+import 'package:timer_builder/timer_builder.dart';
 
 class GraphPoint {
   final int x;
@@ -39,43 +30,49 @@ valuesToPoints(List values, int sensor) {
 }
 
 class HomePage extends StatelessWidget {
-  List<GraphPoint> points0 = valuesToPoints(exampleData[0], 0);
-  List<GraphPoint> points1 = valuesToPoints(exampleData[0], 1);
-  List<GraphPoint> points2 = valuesToPoints(exampleData[0], 2);
-  _getSeriesData() {
-    List<charts.Series<GraphPoint, int>> series = [
-      charts.Series(
-          id: "0",
-          data: points0,
-          domainFn: (GraphPoint series, _) => series.x,
-          measureFn: (GraphPoint series, _) => series.y,
-          colorFn: (GraphPoint series, _) =>
-              charts.MaterialPalette.blue.shadeDefault),
-      charts.Series(
-          id: "1",
-          data: points1,
-          domainFn: (GraphPoint series, _) => series.x,
-          measureFn: (GraphPoint series, _) => series.y,
-          colorFn: (GraphPoint series, _) =>
-              charts.MaterialPalette.red.shadeDefault),
-      charts.Series(
-          id: "2",
-          data: points2,
-          domainFn: (GraphPoint series, _) => series.x,
-          measureFn: (GraphPoint series, _) => series.y,
-          colorFn: (GraphPoint series, _) =>
-              charts.MaterialPalette.green.shadeDefault)
-    ];
-    return series;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: new charts.LineChart(
-        _getSeriesData(),
-        animate: false,
-      ),
-    );
+    int dataPack = 0;
+    return TimerBuilder.periodic(Duration(seconds: 1), //updates every second
+        builder: (context) {
+      List<GraphPoint> points0 = valuesToPoints(exampleData[dataPack], 0);
+      List<GraphPoint> points1 = valuesToPoints(exampleData[dataPack], 1);
+      List<GraphPoint> points2 = valuesToPoints(exampleData[dataPack], 2);
+      dataPack++;
+      if (dataPack == 32) dataPack = 0;
+      getSeriesData() {
+        List<charts.Series<GraphPoint, int>> series = [
+          charts.Series(
+              id: "0",
+              data: points0,
+              domainFn: (GraphPoint series, _) => series.x,
+              measureFn: (GraphPoint series, _) => series.y,
+              colorFn: (GraphPoint series, _) =>
+                  charts.MaterialPalette.blue.shadeDefault),
+          charts.Series(
+              id: "1",
+              data: points1,
+              domainFn: (GraphPoint series, _) => series.x,
+              measureFn: (GraphPoint series, _) => series.y,
+              colorFn: (GraphPoint series, _) =>
+                  charts.MaterialPalette.red.shadeDefault),
+          charts.Series(
+              id: "2",
+              data: points2,
+              domainFn: (GraphPoint series, _) => series.x,
+              measureFn: (GraphPoint series, _) => series.y,
+              colorFn: (GraphPoint series, _) =>
+                  charts.MaterialPalette.green.shadeDefault)
+        ];
+        return series;
+      }
+
+      return Expanded(
+        child: new charts.LineChart(
+          getSeriesData(),
+          animate: false,
+        ),
+      );
+    });
   }
 }
